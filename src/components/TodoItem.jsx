4cc32c9d2510e1c08f8e79ac75a483c1a63ee326
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-07-14 21:31:42 +0530

    Fixed the API URL issues.

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,18 +4,29 @@ import axios from "axios";
 function TodoItem({ todo, setTodos }) {
 
   const handleDelete = async () => {
-  try {
-    await axios.delete(`https://todo-backend-with-node-js-mysql.onrender.com/user/delete/${todo.id}`);
+    try {
+      await axios.delete(`https://todo-backend-with-node-js-mysql.onrender.com/user/delete/${todo.id}`);
 
-    // // Refresh todos
-    // fetchTodos();
+      // // Refresh todos
+      // fetchTodos();
 
-    setTodos(prevTodos => prevTodos.filter(item => item.id !== todo.id));
-  } catch (error) {
-    console.log(error);
-  }
+      setTodos(prevTodos => prevTodos.filter(item => item.id !== todo.id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const toggleComplete = async (id) => {
+    try {
+      const res = await axios.put(`https://todo-backend-with-node-js-mysql.onrender.com/user/edit/${todo.id}`);
+
+      setTodos((prevTodos) => prevTodos.map((item) => item.id === id ? { ...item, completed: res.data.completed } : item
+      )
+      );
+    }
+catch(err){
+  console.log(err)
+}
 };
-
 
   return (
 
@@ -25,11 +36,13 @@ function TodoItem({ todo, setTodos }) {
 
       <div className="icons">
 
-        <FaCheck className={todo.completed ? 'check-mark completed' : 'check-mark'}/>
+        <FaCheck className={todo.completed ? 'check-mark completed' : 'check-mark'}
+          onClick={() => toggleComplete(todo.id)}
+        />
 
         <FaEdit />
 
-        <FaTrash onClick={handleDelete}/>
+        <FaTrash onClick={handleDelete} />
 
       </div>
 
